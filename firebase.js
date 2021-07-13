d22bfc2477e4c7baffc0bcd9fb05043dfd4adec7
valentinewabwire <valentine.wabwire@tracom.co.ke>
2021-07-13 15:12:20 +0300

    initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDg_2VB6pODLbRa8hUpO49jHcejtBleEfc",
+  authDomain: "clone-10480.firebaseapp.com",
+  projectId: "clone-10480",
+  storageBucket: "clone-10480.appspot.com",
+  messagingSenderId: "1051609020225",
+  appId: "1:1051609020225:web:4ad43e02d245793ca893c0",
+};
